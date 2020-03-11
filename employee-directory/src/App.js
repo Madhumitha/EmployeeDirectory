deebc4c75365fc6d5e2cecaef37697058782a12d
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2020-03-11 16:42:26 -0500

    Add Header component

---
+++ employee-directory/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header/Header";
+
+// Style
+import "./components/Header/header.css";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />;
+    </div>
+  );
+}
+
+export default App;
